614c3d90e2c535a669c558178768fe42431c9ccb
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-11-13 19:23:32 +0600

    conditional css style added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+    let message;
+    if (cart.length === 0) {
+        message = <p>Please add some products</p>
+    }
+    else {
+        message = <div>
+            <h3>Rich Kids</h3>
+            <p><small>Thanks for buying</small></p>
+        </div>
+
+    }
     return (
         <div>
-            <h2>Order Summary: {cart.length}</h2>
+            <h2 className={cart.length === 1 ? 'blue' : 'red'}>Order Summary: {cart.length}</h2>
+            <p className={`bold border ${cart.length === 3 ? 'purple' : 'green'}`}>Something</p>
+            {cart.length > 2 ? <span>Buy again</span> : <span>You are poor</span>}
+            {message}
             {
                 cart.map(tshirt => <p
                     key={tshirt._id}
                 >{tshirt.name} <button
                     onClick={() => handleRemoveFromCart(tshirt._id)}
                 >X</button></p>)
+            }
+            {
+                cart.length === 2 && <p>Double Bonus</p>
+            }
+            {
+                cart.length === 3 || <p>Num of Item is not 3</p>
             }
         </div>
     );
