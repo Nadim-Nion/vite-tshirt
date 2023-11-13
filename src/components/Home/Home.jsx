7c980762d45c197babcdd892fbc94ed84a829b1d
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-11-13 16:13:38 +0600

    toast added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,13 +3,29 @@ import { useLoaderData } from 'react-router-dom';
 import TShirt from '../TShirt/TShirt';
 import Cart from '../Cart/Cart';
 import './Home.css';
+import toast from 'react-hot-toast';
+
 
 const Home = () => {
     const tshirts = useLoaderData();
     const [cart, setCart] = useState([]);
 
     const handleAddToCart = tshirt => {
-        console.log(tshirt);
+        const exists = cart.find(shirt => shirt._id === tshirt._id);
+        if (exists) {
+            // alert('You can not add similar item');
+            toast('You can not add similar item');
+        }
+        else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+        }
+
+    }
+
+    const handleRemoveFromCart = id => {
+        const remaining = cart.filter(tshirt => tshirt._id !== id);
+        setCart(remaining);
     }
 
     return (
@@ -24,7 +40,10 @@ const Home = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart
+                    cart={cart}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
             </div>
         </div>
     );
