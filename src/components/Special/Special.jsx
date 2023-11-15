5e3f5877b48bc0cceb3e986986614a8b16512362
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-11-15 15:56:13 +0600

    context api and props drilling

--- src/components/Special/Special.jsx
+++ src/components/Special/Special.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
-const Special = ({ ring }) => {
+const Special = () => {
+    // { ring } object destructuring in the function parameter
+    const token = useContext(RingContext);
     return (
         <div>
             <h2>Special</h2>
-            <p>Ring: {ring}</p>
+            {/* <p>Ring: {ring}</p> */}
+            <p>Ring: {token}</p>
         </div>
     );
 };
--- src/components/Uncle/Uncle.jsx
+++ src/components/Uncle/Uncle.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Cousin from '../Cousin/Cousin';
+import { MoneyContext } from '../Grandpa/Grandpa';
 
 const Uncle = () => {
+    const [money, setMoney] = useContext(MoneyContext);
     return (
         <div>
             <h2>Uncle</h2>
+            <p>Granpa Money: {money}</p>
+            <button onClick={() => setMoney(money + 1000)}>Send Money</button>
             <section className='flex'>
                 <Cousin>Joe</Cousin>
                 <Cousin>Fiona</Cousin>
